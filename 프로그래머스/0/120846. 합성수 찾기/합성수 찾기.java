dffaa165b1633c74832af997e129eb6506e6ea88
hyeyoon23 <109073947+hyeyoon23@users.noreply.github.com>
2026-03-23 18:05:42 +0900

    [level 0] Title: 합성수 찾기, Time: 0.05 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,17 +1,15 @@
 class Solution {
     public int solution(int n) {
-        int count = 0;
-        for (int i = 4; i <= n; i++) {
-            int divisor = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0)
-                    divisor++;
-                if (divisor >= 3) {
-                    count++;
+        int answer = 0;
+
+        for(int i = 4; i <= n; i++){
+            for(int j = 2; j < i; j++){
+                if(i % j == 0){
+                    answer++;
                     break;
                 }
             }
         }
-        return count;
+        return answer;
     }
 }
