52ddb4567b3e83a330f6a17f8c36f26ef0730210
hyeyoon23 <109073947+hyeyoon23@users.noreply.github.com>
2026-04-02 13:55:24 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.49 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        Stack<Integer> stack = new Stack<>();
+        String[] arr = s.split(" ");
+        
+        for (String str : arr) {
+            if (str.equals("Z"))
+                stack.pop();
+            else
+                stack.push(Integer.parseInt(str));
+        }
+        
+        int answer = 0;
+        
+        for (int num : stack) {
+            answer += num;
+        }
+        
+        return answer;
+    }
+}
